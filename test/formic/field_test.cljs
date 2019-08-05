a1fbd951ec71119e594e67b0b3c14629397c668c
Adam Moore <nerdfunk@gmail.com>
2019-08-05 16:57:13 +0900

    more tests

--- test/formic/field_test.cljs
+++ test/formic/field_test.cljs
@@ -1,48 +1,100 @@
 (ns formic.field-test
-  (:require [clojure.test :as t]
+  (:require [cljs.test :as t
+             :refer [testing deftest]]
             [formic.field :as field]
             [formic.components.inputs :as inputs]
             [struct.core :as st]))
 
-(def simple
+(def basic
   {:fields
    [{:field-type :string
-     :id :string-field
+     :id         :string-field
+     :classes    {:input [:added-class1 :added-class2]}
+     :options    {:key "value"}
      :validation [st/required]
      }]})
 
-(t/deftest simple-field-state
-  (let [state @(:state (field/prepare-state simple))]
-    (t/are [x y] (= x (get-in state [0 y]))
-      :string                 :field-type
-      :string-field           :id
-      false                   :touched
-      nil                     :value
-      [st/required]           :validation
-      "String field"          :title
-      inputs/validating-input :component
-      identity                :serializer
-      )))
-
-(def simple-with-default
+(def basic-with-default
   {:fields
    [{:field-type :string
      :id :string-field
      :default "banana"
      }]})
 
-(t/deftest simple-field-default
-  (let [state @(:state (field/prepare-state simple-with-default))]
-    (t/are [x y] (= x (get-in state [0 y]))
-      "banana" :value
-      true     :touched
-      )))
+(deftest basic-fields
+  (testing "default basic field state"
+    (let [state @(:state (field/prepare-state basic))]
+      (t/are [x y] (= x (get-in state [0 y]))
+        :string                                :field-type
+        {:input [:added-class1 :added-class2]} :classes
+        {:key "value"}                         :options
+        :string-field                          :id
+        false                                  :touched
+        nil                                    :value
+        [st/required]                          :validation
+        "String field"                         :title
+        inputs/validating-input                :component
+        identity                               :serializer)))
+  (testing "default value sets value; touched remains false"
+    (let [state @(:state (field/prepare-state basic-with-default))]
+      (t/are [x y] (= x (get-in state [0 y]))
+        "banana" :value
+        false    :touched)))
+  (testing "passing in values sets value; touched is true"
+   (let [state @(:state
+                 (field/prepare-state
+                  basic
+                  {:values {:string-field "apple"}}))]
+     (t/are [x y] (= x (get-in state [0 y]))
+       "apple" :value
+       true    :touched))))
 
-(t/deftest simple-field-value
-  (let [state @(:state
-                (field/prepare-state simple
-                                     {:values {:string-field "apple"}}))]
-    (t/are [x y] (= x (get-in state [0 y]))
-      "apple" :value
-      true    :touched
-      )))
+;; classes
+;; -------------------------------------------------------------------
+
+(def common-title-styles
+  [:title-class])
+
+(def common-input-styles
+  {:title     common-title-styles
+   :label     [:label-class]
+   :input     [:input-class]
+   :error-label [:error-label-class]
+   :error-input [:error-input-class]})
+
+(def email-classes
+  {:input [:email-input-class]})
+
+(def combined-basic-classes
+  {:basic common-input-styles
+   :field-types
+   {:email email-classes}})
+
+(def override-input-classes
+  {:input [:override-input-class]})
+
+(def basic-with-classes
+  {:fields
+   [{:field-type :string
+     :id         :string-field
+     :options    {:key "value"}}
+    {:field-type :email
+     :id         :email-field}
+    {:field-type :email-override
+     :id         :email-field
+     :classes    override-input-classes}]
+   :classes combined-basic-classes})
+
+(deftest basic-field-classes
+  (let [state @(:state (field/prepare-state basic-with-classes))]
+    (testing "classes passed in -- general"
+      (t/is (= common-input-styles
+               (get-in state [0 :classes]))))
+    (testing "specific field classes are combined"
+      (t/is (= (merge common-input-styles
+                      email-classes)
+               (get-in state [1 :classes]))))
+    (testing "overriden classes are combined"
+      (t/is (= (merge common-input-styles
+                      override-input-classes)
+               (get-in state [2 :classes]))))))
