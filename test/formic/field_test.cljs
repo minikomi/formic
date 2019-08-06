5404bcc1cd6d0701361df43054e38a2f2e99fee0
Adam Moore <nerdfunk@gmail.com>
2019-08-06 18:42:32 +0900

    flex tests

--- test/formic/field_test.cljs
+++ test/formic/field_test.cljs
@@ -60,18 +60,18 @@
    :error-label [:error-label-class]
    :error-input [:error-input-class]})
 
-(def email-classes
+(def email-styles
   {:input [:email-input-class]})
 
-(def combined-basic-classes
+(def combined-basic-styles
   {:basic common-input-styles
    :field-types
-   {:email email-classes}})
+   {:email email-styles}})
 
-(def override-basic-classes
+(def override-basic-styles
   {:input [:override-input-class]})
 
-(def basic-with-classes
+(def basic-with-styles
   {:fields
    [{:field-type :string
      :id         :string-field
@@ -80,21 +80,21 @@
      :id         :email-field}
     {:field-type :email-override
      :id         :email-field
-     :classes    override-basic-classes}]
-   :classes combined-basic-classes})
+     :classes    override-basic-styles}]
+   :classes combined-basic-styles})
 
-(deftest basic-field-classes
-  (let [state @(:state (field/prepare-state basic-with-classes))]
+(deftest basic-field-styles
+  (let [state @(:state (field/prepare-state basic-with-styles))]
     (testing "classes passed in -- general"
       (t/is (= common-input-styles
                (get-in state [0 :classes]))))
     (testing "specific field classes are combined"
       (t/is (= (merge common-input-styles
-                      email-classes)
+                      email-styles)
                (get-in state [1 :classes]))))
     (testing "overriden classes are combined"
       (t/is (= (merge common-input-styles
-                      override-basic-classes)
+                      override-basic-styles)
                (get-in state [2 :classes]))))))
 
 ;; Compound
@@ -109,8 +109,8 @@
    :error-label     [:compound-error-label]
    :error-fieldset  [:compound-error-fieldset]})
 
-(def combined-compound-classes
-  (assoc combined-basic-classes
+(def combined-compound-styles
+  (assoc combined-basic-styles
          :compound compound-styles))
 
 (def compound-fields
@@ -124,11 +124,11 @@
        :id         :email-field}
       {:field-type :email-override
        :id         :email-field2}]}]
-   :classes combined-compound-classes})
+   :classes combined-compound-styles})
 
 (def compound-values
   {:string-field "string 1"
-   :email-field "email 1"})
+   :email-field "email@example.com"})
 
 (deftest compound-field
   (testing "compound field general properties"
@@ -143,11 +143,11 @@
       (t/is (= [:string-field :email-field :email-field2]
                (mapv :id (:value f))))))
   (testing "compound field values population"
-    (let [state @(:state (field/prepare-state
-                          compound-fields
-                          {:values {:compound-field compound-values}}))
-          f (first state)
-          f-value (:value f)]
+    (let [f-value (-> @(:state (field/prepare-state
+                             compound-fields
+                             {:values {:compound-field compound-values}}))
+                      first
+                      :value)]
       (t/is (= compound-values
                (->> f-value
                     (map (juxt :id :value))
@@ -155,3 +155,65 @@
                     (into {}))))
       (t/is (= [true true false]
                (mapv :touched f-value))))))
+
+;; Compound
+;; -------------------------------------------------------------------
+
+(def flex-add-styles
+  {:list   [:add-list-class]
+   :item   [:add-item-class]
+   :button [:add-button-class]})
+
+
+(def flex-controls-styles
+  {:wrapper              [:wrapper-class]
+   :move                 [:move-class]
+   :move-disabled        [:move-disabled-class]
+   :move-button          [:move-button-class]
+   :move-button-disabled [:move-button-disabled-class]
+   :delete               [:delete-class]
+   :delete-button        [:delete-button-class]})
+
+(def flex-styles
+  {:fieldset       [:fieldset-class]
+   :err-fieldset   [:err-fieldset-class]
+   :fields-wrapper [:fields-wrapper-class]
+   :title          [:title-class]
+   :add            flex-add-styles
+   :controls       flex-controls-styles
+   :fields-list    [:fields-list-class]
+   :fields-item    [:fields-item-class]
+   :err-wrapper    [:err-wrapper-class]
+   :error-fieldset [:error-fieldset-class]
+   :error-label    [:error-label-class]})
+
+(def combined-flex-styles
+  (assoc combined-compound-styles
+         :flex flex-styles))
+
+(def flex-fields
+  {:fields
+   [{:id :flex-field
+     :flex [:string :email]
+     }]
+   :classes combined-flex-styles})
+
+(def flex-values
+  [{:field-type :string
+    :value "field 1 - string"}
+   {:field-type :string
+    :value "field 2 - string"}
+   {:field-type :email
+    :value "field_3@example.com"}
+   {:field-type :unknown-field-type
+    :value "dropped field - unknown type"}
+   {:field-type :email
+    :value "field_4@example.com"}])
+
+(deftest flex-field
+  (testing "flex field general properties"
+    (let [state @(:state (field/prepare-state flex-fields))
+          f (first state)]
+      (t/is (= [] (:value f)))
+      (t/is (false? (:touched f)))
+      (t/is (= flex-styles (:classes f))))))
